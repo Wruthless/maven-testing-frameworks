81b05520202309e4b3cd2180a56b55b93a980802
Wruthless <wsgnzls@comcast.net>
2023-03-26 11:19:25 -0400

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Using Maven with Testing Frameworks
+Exploring various testing frameworks for use with Maven.
--- src/main/java/org/wruthless/Main.java
+++ src/main/java/org/wruthless/Main.java
@@ -1,4 +1,6 @@
-package org.wruthless;public class Main {
+package org.wruthless;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
