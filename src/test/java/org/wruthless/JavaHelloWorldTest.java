aa87fe90b9022a609c89476db826050b272c2019
Wruthless <wsgnzls@comcast.net>
2023-03-28 14:34:10 -0400

    JUnit4 test example.

---
+++ src/test/java/org/wruthless/JavaHelloWorldTest.java
@@ -0,0 +1,14 @@
+package org.wruthless;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
+}
